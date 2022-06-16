2fe7218f05c005a42e0d2992b21f9207e2766774
root <root@devopsrnd.ibcs-primax.com>
2022-06-16 13:23:27 +0600

    Config

---
+++ planning-idsdp-config/README.md
@@ -0,0 +1,1 @@
+# Planning-IDSDP-Config-Server
---
+++ planning-idsdp-config/src/test/java/com/ibcs/idsdp/PlanningIdsdpConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibcs.idsdp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanningIdsdpConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
